7e3f6c6a81f121ff9431fbd357c3c93048d577bd
Roshan Mishra <roshanmishrarinem@gmail.com>
2019-11-15 20:18:40 +0530

    Add graphs

--- server.py
+++ server.py
@@ -15,20 +15,14 @@ from sklearn import preprocessing
 import pickle
 from flask import Flask,render_template,url_for,request
 
-def predict(fname, year):
-    p_in = open("trained/"+fname+".pkl","rb")
-    model = pickle.load(p_in)
-    x = pd.read_csv("test/"+fname+"_input.csv")
-    a, b, c = float(x[x['YEAR'] == year]['Mar-May']), float(x[x['YEAR'] == year]['avgjune']),float(x[x['YEAR'] == year]['sub'])
-    l = [[a, b, c]]
-    result = model.predict(l)
-    return result
-
 app = Flask(__name__)
+
+
+def preprocess(state, year):
+    pass
 
 @app.route('/predict',methods=['POST'])
 def predict():
-    
     if request.method == 'POST':
         data = request.get_json()
         year = int(data['Year'])
@@ -49,6 +43,18 @@ def predict():
         result['data'] = "Heavy Rainfall"
     print(result)
     return result
+
+@app.route('/rainfall', methods=['POST'])
+def rainfall():
+    if request.method == 'POST':
+        data = request.get_json()
+        year = int(data['Year'])
+        fname = data['State']
+        x_test, y_test = preprocess(fname, year)
+        p_in = open("graphs/"+fname+".pkl","rb")
+        model = pickle.load(p_in)
+        y_pred = model.predict(x_test)
+
 
 if __name__ == '__main__':
 	app.run(debug=True)
